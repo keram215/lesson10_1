27ed9622d9e5d3d637485437a5f2a679cb42a7bc
Sveta <anvisei_serg@mail.ru>
2022-02-17 15:12:02 +0300

    inital commit

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
+from flask import Flask
+import utils
 
-from basicword import BasicWord
-from player import Player
-from utils import load_random_word
+app = Flask(__name__)
+
+@app.route('/',)
+def page_index():
+    candidates = utils.get_candidates_all()
+    page_content = utils.build_performatted_list(candidates)
+    return  page_content
+
+@app.route(("/skill/<skill_name>"))
+def page_skill(skill_name):
+
+    candidates = utils.get_candidates_by_skill(skill_name)
+
+    page_content = utils.build_performatted_list(candidates)
+    return page_content
+
+@app.route("/candidate/<int:vid>")
+
+def page_candidate(vid):
+
+    candidate = utils.get_candidate_by_id(vid)
+
+    page_content = f"<pre>"
+    page_content += f"{candidate['name']} \n"
+    page_content += f"{candidate['position']} \n"
+    page_content += f"{ candidate['skills']} \n"
+    page_content += f"</pre>"
+
+    return page_content
 
 
-def main():
-
-    is_game_on = True
-    print("Введите имя")
-    user_input = input()
-    player = Player(user_input)
-    print(f"Привет {user_input}")
-
-    randow_word = load_random_word()
-    full_word = randow_word.get('word')
-    sub_words = randow_word.get('subwords')
-    basic_word = BasicWord(full_word,sub_words)
-    subword_max_count = basic_word.get_subwords_count()
-
-    print(f'Составте {subword_max_count} слов из слова {basic_word.full_word.upper()}')
-    print(f'Слова должны быть не коросе 3 букв')
-    print(f"Чтобы закончить игру, угадай все слова или нажми stop")
-
-    while is_game_on:
-        user_input = input()
-
-        if user_input == 'stop':
-            break
-        used_count = player.count()
-
-        if used_count == subword_max_count:
-            break
-        #print(basic_word.sub_words)
-        is_subword = basic_word.has_subwords(user_input)
-        was_used = player.is_word_used(user_input)
-
-        if is_subword and not was_used:
-            print('Правильный ответ')
-            player.add_word(user_input)
-        else:
-            print('Неверно')
-
-    print('Программа: слова закончились, игра завершена!')
-    print(f'Программа: вы угадали {player.count()} слова"')
-
-main()
+    return page_content
+app.run()
